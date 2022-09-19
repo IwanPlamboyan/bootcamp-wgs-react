699ed0665eb8d71eaf4bc78a2152a0166aaccb98
IwanPlamboyan <iwanplam03@gmail.com>
2022-09-19 10:19:35 +0700

    menyimpan navbar di file terpisah

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
+// mengimport component navbar
+import Navbar from './component/Navbar';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://avatars.githubusercontent.com/u/67259922?v=4" className="App-logo" alt="logo" />
-        <h1>Bootcamp batch 3</h1>
-        <h2>Iwan Plamboyan</h2>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      <Navbar />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 
-// mengambil document file html di folder public dengan id root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = (
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">
-        Navbar
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">
-              Home
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">
-              About
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">
-              contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-// merender element di
-root.render(element);
